39709bdaba10cb2ec28934916103f0ba5b955898
Nick Cruz <ncruz@umich.edu>
2020-01-09 22:38:54 -0800

    Update .gitignore and clear .gitignore'd cache.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+
 plugins {
     kotlin("jvm") version "1.3.11"
 }
@@ -9,10 +10,14 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
     compile(kotlin("stdlib-jdk8"))
+
+    testCompile("junit:junit:4.12")
+    testCompile("io.strikt:strikt-core:0.23.4")
 }
 
 tasks.withType<KotlinCompile> {
