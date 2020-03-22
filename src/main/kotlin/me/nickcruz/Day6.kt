8c37c89d237fa68d1265997c5434beef219d834a
Nick Cruz <ncruz@umich.edu>
2020-03-22 15:45:42 -0700

    Complete Day6 part 2.
    This worked out really well! Luckily, I had already done the work of
    creating the orbit tree (even though I only needed to _traverse_ it in
    part 1).
    
    countOrbitalTransfers could definitely be cleaned up a bit...but it
    works.

--- src/main/kotlin/me/nickcruz/Day6.kt
+++ src/main/kotlin/me/nickcruz/Day6.kt
@@ -1,5 +1,9 @@
 package me.nickcruz
 
+import java.util.LinkedList
+import java.util.Queue
+import kotlin.math.min
+
 /**
  * --- Day 6: Universal Orbit Map ---
  *
@@ -64,10 +68,120 @@ package me.nickcruz
  * The total number of direct and indirect orbits in this example is 42.
  *
  * What is the total number of direct and indirect orbits in your map data?
+ *
+ * --- Part Two ---
+ * Now, you just need to figure out how many orbital transfers you (YOU) need to take to get to Santa (SAN).
+ *
+ * You start at the object YOU are orbiting; your destination is the object SAN is orbiting. An orbital transfer lets
+ * you move from any object to an object orbiting or orbited by that object.
+ *
+ * For example, suppose you have the following map:
+ *
+ * COM)B
+ * B)C
+ * C)D
+ * D)E
+ * E)F
+ * B)G
+ * G)H
+ * D)I
+ * E)J
+ * J)K
+ * K)L
+ * K)YOU
+ * I)SAN
+ * Visually, the above map of orbits looks like this:
+ *
+ *                           YOU
+ *                          /
+ *         G - H       J - K - L
+ *        /           /
+ * COM - B - C - D - E - F
+ *               \
+ *                I - SAN
+ *
+ * In this example, YOU are in orbit around K, and SAN is in orbit around I. To move from K to I, a minimum of 4 orbital
+ * transfers are required:
+ *
+ * K to J
+ * J to E
+ * E to D
+ * D to I
+ * Afterward, the map of orbits looks like this:
+ *
+ *         G - H       J - K - L
+ *        /           /
+ * COM - B - C - D - E - F
+ *               \
+ *                I - SAN
+ *                \
+ *                 YOU
+ *
+ * What is the minimum number of orbital transfers required to move from the object YOU are orbiting to the object SAN
+ * is orbiting? (Between the objects they are orbiting - not between YOU and SAN.)
  */
 class Day6 {
 
     fun countOrbits(orbitMap: List<String>): Int {
+        var totalOrbits = 0
+        assembleCelestialBodies(orbitMap) {
+            totalOrbits += it.orbitDepth
+        }
+        return totalOrbits
+    }
+
+    fun countOrbitalTransfers(orbitMap: List<String>, start: String, end: String): Int {
+        // Find start and start there. Starting point is actually start's center
+        var startingBody: CelestialBody? = null
+        var endingBody: CelestialBody? = null
+        val celestialBodies = assembleCelestialBodies(orbitMap) {
+            when (it.name) {
+                start -> startingBody = it.center
+                end -> endingBody = it.center
+            }
+        }
+        val requireStart = startingBody ?: throw IllegalStateException("StartingBody is null: start: $start")
+        val requireEnd = endingBody ?: throw IllegalStateException("EndingBody is null: end: $end")
+
+        // BFS through graph until you find the ending body
+        val traversed = mutableSetOf(start, requireStart.name)
+        val pendingTransfers: Queue<Transfer> = LinkedList()
+        pendingTransfers.add(Transfer(requireStart, 0))
+
+        var minTransfers: Int? = null
+
+        while (pendingTransfers.isNotEmpty()) {
+            val currentTransfer = pendingTransfers.remove()
+
+            // Min the completed transfer distances.
+            if (currentTransfer.current == requireEnd) {
+                val completedTransferDistance = currentTransfer.transfersSoFar
+                minTransfers = min(minTransfers ?: Int.MAX_VALUE, completedTransferDistance)
+                continue
+            }
+
+            // Add all remaining possible orbits (+ parent) to queue.
+            val bodiesToCheck = currentTransfer.current.orbits
+                .filter { !traversed.contains(it.name) }
+                .toMutableList()
+
+            currentTransfer.current.center?.let {
+                bodiesToCheck.add(it)
+            }
+
+            pendingTransfers.addAll(bodiesToCheck
+                .onEach { traversed.add(it.name) }
+                .map { Transfer(it, currentTransfer.transfersSoFar + 1) })
+        }
+
+        // Return how many transfers you did
+        return minTransfers ?: throw IllegalStateException("Couldn't find $end's center.")
+    }
+
+    private fun assembleCelestialBodies(
+        orbitMap: List<String>,
+        onCelestialBodyCreated: (CelestialBody) -> Unit = {}
+    ): CelestialBody {
         // Assemble strings into pairs, where each center points to its orbits.
         val orbitPairs = orbitMap.map { orbitPair ->
             val objectPair = orbitPair.split(ORBIT_DELIMITER)
@@ -86,36 +200,71 @@ class Day6 {
 
         // Recursively assemble a tree:
         // Start at Center Of Mass, then add all orbits
-        var totalOrbits = 0
-        createCelestialBody(centerToOrbits, CENTER_OF_MASS, 0) {
-            totalOrbits += it.orbitDepth
-        }
-
-        return totalOrbits
+        return createCelestialBody(
+            centerToOrbits = centerToOrbits,
+            name = CENTER_OF_MASS,
+            orbitDepth = 0,
+            center = null,
+            onCelestialBodyCreated = onCelestialBodyCreated
+        )
     }
 
     private fun createCelestialBody(
         centerToOrbits: Map<String, List<String>>,
         name: String,
         orbitDepth: Int,
-        onCelestialObjectCreated: (CelestialObject) -> Unit
-    ): CelestialObject {
-        val orbits = centerToOrbits[name] ?: emptyList()
-        return CelestialObject(
-            name,
-            orbits.map { createCelestialBody(centerToOrbits, it, orbitDepth + 1, onCelestialObjectCreated) },
-            orbitDepth
-        ).apply(onCelestialObjectCreated)
+        center: CelestialBody? = null,
+        onCelestialBodyCreated: (CelestialBody) -> Unit = {}
+    ): CelestialBody {
+        val orbitNames = centerToOrbits[name] ?: emptyList()
+
+        return CelestialBody(
+            name = name,
+            orbitDepth = orbitDepth,
+            center = center
+        )
+            .apply {
+                orbits = orbitNames.map { orbitName ->
+                    createCelestialBody(
+                        centerToOrbits = centerToOrbits,
+                        name = orbitName,
+                        orbitDepth = orbitDepth + 1,
+                        center = this,
+                        onCelestialBodyCreated = onCelestialBodyCreated
+                    )
+                }
+            }
+            .apply(onCelestialBodyCreated)
     }
 }
 
 private const val ORBIT_DELIMITER = ')'
 private const val CENTER_OF_MASS = "COM"
 
-data class CelestialObject(
+data class Transfer(val current: CelestialBody, val transfersSoFar: Int = 0)
+
+class CelestialBody(
     val name: String,
-    val orbits: List<CelestialObject> = emptyList(),
+    var center: CelestialBody? = null,
+    var orbits: List<CelestialBody> = emptyList(),
 
     // Number of orbits from center of mass, which is basically the depth of the celestial object in a tree.
     val orbitDepth: Int = 0
-)
+
+
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as CelestialBody
+
+        if (name != other.name) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return name.hashCode()
+    }
+}
